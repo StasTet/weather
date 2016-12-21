30600f8d087ee54a718583f11a3ebee700316dcb
Stas <stastetin@gmail.com>
2016-12-21 12:55:01 +0300

    using new API

--- src/script.js
+++ src/script.js
@@ -30,18 +30,22 @@ $(document).ready(function(){
 	});
 
 	function ajaxFanc(){
-		$.ajax({
-			url: "http://api.openweathermap.org/data/2.5/weather?q=" + cityName + "&units=metric&appid=862dc7cbbdc5915f4aa2bdec7b31dbdd",
+		$.simpleWeather({
+			location: cityName,
+			unit: 'c',
+			//url: "http://api.openweathermap.org/data/2.5/weather?q=" + cityName + "&units=metric&appid=862dc7cbbdc5915f4aa2bdec7b31dbdd",
 			success:function(result){
 				console.log(result);
 				//<hr class="featurette-divider">
-				$('#weather .cityName').html(result.name);
-				$('#weather .temp').html('<img src="src/image/temperature.png" class="img-circle">' + result.main.temp + '&deg;');
-				$('#weather .sky').html(result.weather[0].main);
-				$('#weather .wind').html(result.wind.speed + ' km/h');
+				$('#weather .cityName').html(result.city);
+				$('#weather .temp').html('<img src="src/image/temperature.png" class="img-circle">' + result.temp + '&deg;');
+				$('#weather .sky').html(result.currently);
+				//$('#weather .image').html('<img src="' + result.image + '" />');
+				$('#weather .wind').html(result.wind.speed + 'km/h');
 			},
-			error:function(){
-				alert('City "' + cityName + '" does not exist');
+			error:function(error){
+				//alert('City "' + cityName + '" does not exist');
+				console.log(error);
 			}
 			
 		});
